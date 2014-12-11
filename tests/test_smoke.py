e84f62fe91d4bdb8710b4b662b04cdf181f94cb9
Tristan Wietsma <tristan.wietsma@incontextsolutions.com>
2014-12-11 12:32:45 -0600

    context

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,7 +1,13 @@
+import unittest
 from flask import Flask
 from flask.ext.goat import Goat
 
 
-def test_smoke():
-    app = Flask(__name__)
-    Goat(app)
+class TestSmoke(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+
+    def test_smoke(self):
+        with self.app.app_context():
+            Goat(self.app)
