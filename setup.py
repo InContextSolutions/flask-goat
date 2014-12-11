3b77208bdfb3f586168c9b3c378e201796e136c6
Tristan Wietsma <tristan.wietsma@incontextsolutions.com>
2014-12-11 11:35:14 -0600

    fixed setup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     include_package_data=True,
     platforms='any',
     install_requires=[
-        'Flask'
+        'Flask',
         'redis',
         'simplejson',
         'requests',
