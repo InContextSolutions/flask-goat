ee1d8a02617504aad5e1be9e758a69f1a7d73424
Tristan Wietsma <tristan.wietsma@incontextsolutions.com>
2014-12-11 09:25:35 -0600

    added setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-Goat
+-------------
+
+Flask-Goat is a plugin for to support security and user administration via GitHub OAuth and organization structure. The plugin assumes membership in an organization as a basic requirement for authorization. User privilege level can be determined from team membership. A request context is built to support these concepts and can be used by endpoints. The plugin is intended to implemented via the 'before_request' handler.
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-Goat',
+    version='0.0',
+    url='http://incontextsolutions.github.io/flask-goat/',
+    license='MIT',
+    author='Tristan Wietsma',
+    author_email='tristan.wietsma@incontextsolutions.com',
+    description='Flask plugin for security and user administration via GitHub OAuth & organization',
+    long_description=__doc__,
+    py_modules=['flask_goat'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
