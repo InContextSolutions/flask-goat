abe9248c2f34f6039f44ef3877910dd05ae849f4
Tristan Wietsma <tristan.wietsma@incontextsolutions.com>
2014-12-11 12:48:16 -0600

    get url

--- tests/test_goat.py
+++ tests/test_goat.py
@@ -7,6 +7,7 @@ class TestGoat(unittest.TestCase):
 
     def setUp(self):
         self.app = Flask(__name__)
+        self.app.config.setdefault('GOAT_CLIENT_ID', "publicid")
 
     def test_smoke(self):
         with self.app.app_context():
@@ -17,3 +18,10 @@ class TestGoat(unittest.TestCase):
             g = Goat(self.app)
             g.save_state('test')
             self.assertTrue(g.is_valid_state('test'))
+
+    def test_make_auth_url(self):
+        with self.app.app_context():
+            g = Goat(self.app)
+            go_to = 'https://example.com'
+            url = g.test_make_auth_url(go_to)
+            self.assertEqual(url, 'xxxx')
