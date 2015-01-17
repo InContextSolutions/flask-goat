96ff113578e17ee3a9c09330209dff9261d69774
Tristan Wietsma <tristanwietsma@users.noreply.github.com>
2015-01-17 13:57:54 -0600

    version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Goat',
-    version='0.2.0',
+    version='0.2.1',
     url='http://incontextsolutions.github.io/flask-goat/',
     license='MIT',
     author='Tristan Wietsma',
